04ee6897dd40691ebf56fae49d0fc35bf5928be8
MarkLewisII <mark.lewis070301@gmail.com>
2023-09-04 13:38:00 -0400

    Add computer move choice and playRound function

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+function getComputerChoice() {
+	let computerChoice = Math.floor(Math.random() * 3) + 1;
+
+	if (computerChoice == 1) {
+		return "rock";
+	} else if (computerChoice == 2) {
+		return "paper";
+	} else {
+		return "scissors";
+	}
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
+
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection == computerSelection) {
+		console.log("Draw");
+	}
+
+	if (
+		(playerSelection == "rock" && computerSelection == "paper") ||
+		(playerSelection == "paper" && computerSelection == "scissors") ||
+		(playerSelection == "scissors" && computerSelection == "rock")
+	) {
+		console.log(
+			"You Lose! " + computerSelection + " beats " + playerSelection
+		);
+	}
+
+	if (
+		(playerSelection == "rock" && computerSelection == "scissors") ||
+		(playerSelection == "paper" && computerSelection == "rock") ||
+		(playerSelection == "scissors" && computerSelection == "paper")
+	) {
+		console.log(
+			"You Win! " +
+				playerSelection +
+				" beats " +
+				computerSelection
+		);
+	}
+}
+
+playRound(playerSelection, computerSelection);
