c69c5d8c9c519686804b8973e0fccea27310c7b0
MarkLewisII <mark.lewis070301@gmail.com>
2023-10-17 14:40:13 -0400

    Adds a display of current scores

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+var playerScore = 0;
+var computerScore = 0;
+
 function getComputerChoice() {
 	let computerChoice = Math.floor(Math.random() * 3) + 1;
 
@@ -12,7 +15,7 @@ function getComputerChoice() {
 
 function playRound(playerSelection) {
 	let computerSelection = getComputerChoice();
-	let outcome = document.querySelector('.result');
+	let outcome = document.querySelector(".result");
 
 	if (playerSelection == computerSelection) {
 		// console.log("Draw");
@@ -28,6 +31,8 @@ function playRound(playerSelection) {
 		// 	"You Lose! " + computerSelection + " beats " + playerSelection
 		// );
 		outcome.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+
+		computerScore++;
 	}
 
 	if (
@@ -39,9 +44,61 @@ function playRound(playerSelection) {
 		// 	"You Win! " + playerSelection + " beats " + computerSelection
 		// );
 		outcome.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+
+		playerScore++;
+	}
+
+	UpdateScoreboard(playerScore, computerScore);
+}
+
+function UpdateScoreboard(playerScore, computerScore) {
+	document.getElementById(
+		"playerScore"
+	).textContent = `Player : ${playerScore}`;
+	document.getElementById(
+		"computerScore"
+	).textContent = `Computer : ${computerScore}`;
+
+	EndScreen(playerScore, computerScore);
+}
+
+function EndScreen(playerScore, computerScore) {
+	let finalResult = document.getElementById("winner");
+
+	if (playerScore >= 5) {
+		finalResult.textContent = "Player Wins!";
+		Reset();
+		
+	} else if (computerScore >= 5) {
+		finalResult.textContent = "Computer Wins!";
+		Reset();
 	}
 }
 
+function Reset() {
+	document.getElementById('rockBttn').disabled = true;
+	document.getElementById('paperBttn').disabled = true;
+	document.getElementById('scissorBttn').disbled = true;
+
+	let resetBttn = document.createElement('button');
+
+	resetBttn.textContent = "Reset";
+	resetBttn.id = "resetBttn";
+
+	resetBttn.onclick = () =>
+	{
+		let self = document.getElementById('resetBttn');
+
+		playerScore = 0;
+		computerScore = 0;
+
+		document.getElementById("rockBttn").disabled = false;
+		document.getElementById("paperBttn").disabled = false;
+		document.getElementById("scissorBttn").disbled = false;
+		
+		self.parentNode.removeChild('resetBttn');
+		}
+}
 // function game() {
 // 	for (let i = 0; i < 5; i++) {
 // 		const playerSelection = prompt(
